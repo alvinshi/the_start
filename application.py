9816e7570725bc6cb00c5ead71067eebe0a249d2
Alvin Shi <xiangs1@andrew.cmu.edu>
2020-04-17 21:57:50 -0700

    Basic request and cookie

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, make_response
 from markupsafe import escape
 
 app = Flask(__name__)
 
+BIG_SECRET = "THIS IS A BIG SECRET"
+
 @app.route('/')
 def index():
-    return 'Index Page'
+    if request.cookies.get('secret') == BIG_SECRET:
+        return hello('old friend')
+    return login()
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -18,6 +22,20 @@ def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % escape(username)
 
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login_submit/', methods=['POST'])
+def login_submit():
+    if valid_login(request.form['fname'],
+                   request.form['fpassword']):
+        resp = make_response(hello(request.form['fname']))
+        resp.set_cookie('secret', BIG_SECRET)
+        return resp
+    else:
+        return login() 
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
@@ -27,3 +45,6 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
+
+def valid_login(username, password):
+    return username == "Alvin"
